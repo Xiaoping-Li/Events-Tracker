b3f5923abf16eb2c2f3cea63092e66ba1d029585
Xiaoping Li <narci2008@gmail.com>
2018-07-14 21:44:06 -0700

    Initiate Navigation Component

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div className="App">
+        Welcome to Events-Tracker Navigation Page
+      </div>
+    );
+  }
+}
+
+export default Navigation;
