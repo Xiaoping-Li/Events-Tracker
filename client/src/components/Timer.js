a50c61b0693c01d631a56ed75466e8152aea2429
Xiaoping Li <narci2008@gmail.com>
2018-07-16 09:40:54 -0700

    Add Timer Component

---
+++ client/src/components/Timer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Timer extends Component {
+  render() {
+    return (
+      <div>
+        Welcome to Events-Tracker Timer Page
+      </div>
+    );
+  }
+}
+
+export default Timer;
