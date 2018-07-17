2833717daa9f68fc7a8d00098446546dbf8e2e44
Xiaoping Li <narci2008@gmail.com>
2018-07-17 10:20:38 -0700

    Events Component present eventsList

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import TimeSlot from './TimeSlot';
 
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
-        Welcome to Events-Tracker Home Page
+        Welcome to Events-Tracker Event Page
+
+        <TimeSlot />
       </div>
     );
   }
--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import ROOT_URL from '../utils/config';
 
 import EventForm from './EventForm';
+import Event from './Event';
 
 class Events extends Component {
   constructor() {
@@ -10,10 +13,28 @@ class Events extends Component {
     };
   }
 
+  componentDidMount() {
+    axios.get(ROOT_URL + '/api/events/')
+      .then(result => {
+        const detail = result.data;
+        this.setState({ eventsList: detail });
+        console.log(result);
+      })
+      .catch(error => {
+        console.log({ error, message: 'failed to load events' });
+      })
+  }
+
   render() {
+    const events = this.state.eventsList.map((event, i) => <li>{<Event key={i} event={event} />}</li>);
     return (
       <div>
-        <EventForm />    
+        <EventForm />
+        <div className="events__List">
+          <ul>
+            {events}
+          </ul>  
+        </div>    
       </div>
     );
   }
