b19205a8f9128de939afcc47fcf22acbcbd8a012
Xiaoping Li <narci2008@gmail.com>
2018-07-17 09:56:34 -0700

    TimeSlot Component could add new time slot to DB

--- client/src/components/TimeSlot.js
+++ client/src/components/TimeSlot.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import ROOT_URL from '../utils/config';
 
 class TimeSlot extends Component {
   constructor(props) {
@@ -6,21 +8,46 @@ class TimeSlot extends Component {
     this.state = {
       start: 0,
       stop: 0,
-      eventID: null,
+      eventID: 1, //dummy eventID 1
     }; 
   }
 
-  handleButtonClick = (e) => {
+  handleTimerClick = (e) => {
     this.setState({
       [e.target.name]: e.target.value
+    });
+  }
+
+  handleAddTimeSlot = (e) => {
+    e.preventDefault();
+
+    const newTimeSlot = {
+      start: this.state.start,
+      stop: this.state.stop,
+      eventID: 1,
+    };
+
+    axios.post(ROOT_URL + '/api/timeSlots/', newTimeSlot)
+      .then(result => {
+        console.log('Add newTimeSlot to DB');
+      })
+      .catch(error => {
+        console.log({ error, message: 'failed to add newTimeSlot' });
+      });
+
+    this.setState({
+      start: 0,
+      stop: 0,
+      eventID: 1,
     });
   }
 
   render() {
     return (
       <div>
-        <button name="start" value={new Date().getTime()} onClick={this.handleButtonClick}>Start</button>
-        <button name="stop" value={new Date().getTime()} onClick={this.handleButtonClick}>Stop</button>
+        <button name="start" value={new Date().getTime()} onClick={this.handleTimerClick}>Start</button>
+        <button name="stop" value={new Date().getTime()} onClick={this.handleTimerClick}>Stop</button>
+        <button onClick={this.handleAddTimeSlot}>Add</button>
         {this.state.start} /
         {this.state.stop}
         <div>
