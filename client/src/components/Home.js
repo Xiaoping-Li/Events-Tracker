525b045865138ff67521a90838f266ad808d7ae2
Xiaoping-Li <narci2008@gmail.com>
2018-07-21 12:24:06 -0700

    Add history.js under src folder

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Auth from './Auth/Auth.js';
 
 
 class Home extends Component {
+  
+
   render() {
+    const auth = new Auth();
     return (
-      <div className="App">
+      <div>
+        {auth.login()}
         Welcome to Events-Tracker Home Page
       </div>
     );
