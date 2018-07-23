4ea871598650f4c282e83fe6e4e8aa3cf8b11e57
Xiaoping Li <narci2008@gmail.com>
2018-07-23 14:51:25 -0700

    Modify Home.js and Navigation.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import Auth from '../Auth/Auth.js';
-
 
 class Home extends Component {
-  
 
   render() {
-    const auth = new Auth();
-    auth.login();
     return (
       <div>
-        Welcome to Events-Tracker Home Page
+        Welcome to Home page!
       </div>
     );
   }
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navigation extends Component {
+
   dynamicLinks() {
     if (this.props.auth.isAuthenticated) {
       return [
         <Link key={1} to='/home'>Home</Link>,
         <Link key={2} to='/events'>Events</Link>,
-        <Link key={3} to='/home'>Log Out</Link>,
+        <Link key={3} to='/home' onClick={this.props.auth.logout()}>Log Out</Link>,
       ];
     } else {
       return [
         <Link key={1} to='/home'>Home</Link>,
-        <Link key={2} to='/home'>Log In</Link>,
+        <Link key={2} to='/home' onClick={this.props.auth.login()}>Log In</Link>,
       ];
     }
   }
