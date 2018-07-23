237ab417c06daa4e7501587ddfeeb4bbcac1c917
Xiaoping Li <narci2008@gmail.com>
2018-07-23 10:12:20 -0700

    Config Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,16 +1,18 @@
-// import auth0 from 'auth0-js';
+import history from '../../history';
+import auth0 from 'auth0-js';
+import config from '../../utils/config';
 
-// export default class Auth {
-//   auth0 = new auth0.WebAuth({
-//     domain: 'xiaoping-li.auth0.com',
-//     clientID: '1xHelLx5BWNDw8KPgV3XaXzqL3VQWfuV',
-//     redirectUri: 'http://localhost:3000/callback',
-//     audience: 'https://xiaoping-li.auth0.com/userinfo',
-//     responseType: 'token id_token',
-//     scope: 'openid'
-//   });
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: config.domain,
+    clientID: config.clientID,
+    redirectUri: config.callbackURL,
+    audience: `https://${config.domain}/userinfo`,
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
 
-//   login() {
-//     this.auth0.authorize();
-//   }
-// }
+  login() {
+    this.auth0.authorize();
+  }
+}
