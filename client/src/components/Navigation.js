2e532bcf809a547986461ab4a175dceb76241e47
Xiaoping Li <narci2008@gmail.com>
2018-07-23 13:51:00 -0700

    Add dynamicLinks to Navigation component

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navigation extends Component {
+  dynamicLinks() {
+    if (this.props.auth.isAuthenticated) {
+      return [
+        <Link key={1} to='/home'>Home</Link>,
+        <Link key={2} to='/events'>Events</Link>,
+        <Link key={3} to='/home'>Log Out</Link>,
+      ];
+    } else {
+      return [
+        <Link key={1} to='/home'>Home</Link>,
+        <Link key={2} to='/home'>Log In</Link>,
+      ];
+    }
+  }
+
   render() {
     return (
       <div>
-        <Link key={1} to='/home'>Home</Link>
-        <Link key={2} to='/events'>Events</Link>
+        {this.dynamicLinks()}
       </div>
     );
   }
