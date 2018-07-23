ef73301491df33a6b44d2ca3f51b861f5cd835b8
Xiaoping Li <narci2008@gmail.com>
2018-07-23 13:31:40 -0700

    Modify Routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
 import Auth from './Auth/Auth.js';
 import history from './history';
-
+// import Callback from './Callback/Callback';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
 import EventsList from './components/EventsList';
@@ -18,9 +19,9 @@ class App extends Component {
     return (
       <BrowserRouter history={history}>
         <div className="App">
-          <Navigation />
-          <Route path='/home' exact component={Home} />
-          <Route path='/events' component={EventsList} />
+          <Navigation auth={auth}/>
+          <Route path='/home' exact render={(props) => <Home auth={auth} {...props} />} />
+          <Route path='/events' render={(props) => <EventsList auth={auth} {...props} />} />
           <Route path='/:id/eventDetails' component={EventDetails} />
         </div>
       </BrowserRouter> 
