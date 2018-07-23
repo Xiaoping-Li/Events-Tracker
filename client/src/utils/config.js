7be35eb2f7e207f597cd9a71c2ea58cd8bbc4dbe
Xiaoping Li <narci2008@gmail.com>
2018-07-23 08:37:36 -0700

    Clean config.js

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,6 +1,4 @@
-// const config = {
-//   ROOT_URL: process.env.REACT_APP_ROOT_URL || 'http://localhost:5000'
-// }
+
 
 
 const ROOT_URL = process.env.REACT_APP_ROOT_URL || 'http://localhost:5000';
